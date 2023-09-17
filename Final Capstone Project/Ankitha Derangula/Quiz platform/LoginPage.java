264bc1bc65813766e5c2469ceb4622f815a385d7
Ankitha-Derangula <ankitha8867@gmail.com>
2023-09-17 17:57:25 +0530

    mini and Final
    MIni Projects and Final Project submitted

---
+++ Final Capstone Project/Ankitha Derangula/Quiz platform/LoginPage.java
@@ -0,0 +1,80 @@
+package quiz;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//ActiveListener-interface
+
+public class Loginpage extends JFrame implements ActionListener{
+    JButton back,next;
+    JTextField UserName;
+    Loginpage(){
+        // Background
+        getContentPane().setBackground(Color.white);
+        setLayout(null);
+        ImageIcon icon=new ImageIcon("quiz.jpg");
+        JLabel image=new JLabel(icon);
+        image.setBounds(0,0,590,515);
+        add(image);
+
+        //Heading
+        JLabel heading=new JLabel("Brainly Java Quiz");
+        heading.setBounds(650,50,500,45);
+        heading.setFont(new Font("Lucida",Font.BOLD,40));
+        heading.setForeground(new Color(106,0,119));
+        add(heading);
+
+        //Name
+        JLabel name=new JLabel("Enter Your Name");
+        name.setBounds(750,130,500,30);
+        name.setFont(new Font("Lucida Bright",Font.BOLD,20));
+        name.setForeground(new Color(0,0,0));
+        add(name);
+
+        //Text Area
+        UserName=new JTextField();
+        UserName.setBounds(600,200,500,45);
+        UserName.setFont(new Font("Lucida Bright",Font.BOLD,20));
+        UserName.setForeground(new Color(0,0,0));
+        add(UserName);
+
+        //Buttons
+        next=new JButton("Next Page >>");
+        next.setBounds(600,300,250,40);
+        next.setFont(new Font("Lucida Bright",Font.BOLD,15));
+        next.setBackground(new Color(106,0,119));
+        next.setForeground(new Color(255,255,255));
+        next.addActionListener(this);
+        add(next);
+
+        back=new JButton("Back");
+        back.setBounds(900,300,200,40);
+        back.setFont(new Font("Lucida Bright",Font.BOLD,15));
+        back.setBackground(new Color(106,0,119));
+        back.setForeground(new Color(255,255,255));
+        back.addActionListener(this);
+        add(back);
+
+
+        //Fram code
+        setLocation(50,100);
+        setSize(1200,550);
+        setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==next) {
+            String Uname=UserName.getText();  // your name
+            setVisible(false);
+            new Rulespage(Uname);
+        }
+        else if(e.getSource()==back) {
+            setVisible(false);
+        }
+    }
+
+    public static void main(String[] args){
+        new Loginpage();
+    }
+}
---
+++ Mini Projects/Ankitha Derangula/Currency Converter.java
@@ -0,0 +1,98 @@
+package currency;
+import java.util.*;
+public class CurrencyConverter{
+public static void main(String []args){
+double dollar,pond,euro,yen,ringgit,rupiah,won,dong,lkr,npr;
+Scanner sc=new Scanner(System.in);
+System.out.println("please enter the Amount in rupee:");
+double Amount=sc.nextDouble();
+System.out.println("Choose the currency to converter");
+System.out.println("Enter 1 :Dollar");
+System.out.println("Enter 2 :Pound");
+System.out.println("Enter 3 :Euro");
+System.out.println("Enter 4 :Yen");
+System.out.println("Enter 5 :Ringgit");
+System.out.println("Enter 6 :rupiah");
+System.out.println("Enter 7 :won");
+System.out.println("Enter 8 :dong");
+System.out.println("Enter 9 :lkr");
+System.out.println("Enter 10 :npr");
+int choice=sc.nextInt();
+switch(choice){
+case 1:
+{
+ doller=Amount/82.8;
+System.out.println("Converting"+Amount+"Rupees into Dollar: 
+"+String.format("%.2f",dollar)+ " Dollars");
+break;
+}
+case 2:
+{
+ pond=Amount/105.6;
+System.out.println("Converting"+Amount+"Rupees into pound: 
+"+String.format("%.2f",pond)+ " pounds");
+break;
+}
+case 3:
+{
+ euro=Amount/91;
+System.out.println("Converting"+Amount+"Rupees into euro: 
+"+String.format("%.2f",euro)+ " euros");
+break;
+}
+case 4:
+{
+ yen=Amount/0.58;
+System.out.println("Converting"+Amount+"Rupees into yen: 
+"+String.format("%.2f",yen)+ " yen");
+break;
+}
+case 5:
+{
+ ringgit=Amount/18.15;
+System.out.println("Converting"+Amount+"Rupees into ringgit: 
+"+String.format("%.2f",ringgit)+ " Ringgit");
+break;
+}
+case 6:
+{
+rupiah =Amount/0.005;
+System.out.println("Converting"+Amount+"Rupees into Doller: 
+"+String.format("%.2f",rupiah)+ " Dollers");
+break;
+}
+case 7:
+{
+ won=Amount/0.06;
+System.out.println("Converting"+Amount+"Rupees into won: 
+"+String.format("%.2f",won)+ " wons");
+break;
+}
+case 8:
+{
+ dong=Amount/0.003;
+System.out.println("Converting"+Amount+"Rupees into dong: 
+"+String.format("%.2f",dong)+ " Dong");
+break;
+}
+case 9:
+{
+ lkr=Amount/0.25;
+System.out.println("Converting"+Amount+"Rupees into lkr: 
+"+String.format("%.2f",lkr)+ " lkr");
+break;
+}
+case 10:
+{
+npr=Amount/0.62;
+System.out.println("Converting"+Amount+"Rupees into npr: 
+"+String.format("%.2f",npr)+ " npr");
+break;
+}
+default:
+{
+ System.out.println(" Invalid input: please enter valid input");
+}
+}
+}
+}
